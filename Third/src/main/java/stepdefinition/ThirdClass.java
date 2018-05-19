da40559a9afa0bfb60a68fdf346bcd68d6e25584
Nitesh <majji.nitesh@gmail.com>
2018-05-20 00:57:06 +0530

    Committing after cucumber integration

---
+++ Third/src/main/java/stepdefinition/ThirdClass.java
@@ -0,0 +1,39 @@
+package stepdefinition;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import cucumber.api.java.en.Then;
+
+public class ThirdClass {
+
+	WebDriver dr = new FirefoxDriver();
+
+	public ThirdClass(){
+
+	}
+
+	@Then("User is able to launch firefox and redirect to facebook$")
+	public void launchBrowser(){
+
+		dr.get("https://www.facebook.com");
+	}
+
+	@Then("User enters username and password in the search text box$")
+	public void enterTextSearchBox() throws InterruptedException{
+		dr.manage().window().maximize();
+		Thread.sleep(4000);
+		dr.findElement(By.id("email")).sendKeys("test@test.com");
+
+		dr.findElement(By.id("pass")).sendKeys("test");
+	}
+
+	@Then("User click on login button$")
+	public void clickSearchButton(){
+
+		dr.findElement(By.id("u_0_2")).click();
+
+	}
+
+}
---
+++ Third/src/main/resources/Features/LaunchGoogle.feature
@@ -0,0 +1,10 @@
+#Author: Nitesh Majji
+#Date : 19th May 2018
+
+Feature: LaunchGoogle
+  Objective of this feature file is to launch google in firefox using selenium webdriver
+
+  Scenario: Launch Google
+    Given User is able to launch firefox and redirect to facebook
+    And User enters username and password in the search text box
+    When User click on login button
